a28a177234ca33a67a174741f994a8cdebbdd136
Abhilash Kumar Giri <114879092+Abhi-lash19@users.noreply.github.com>
2025-12-29 15:53:38 +0000

    chore: initialize Node.js project with Express, TypeScript, and basic server

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+//src/server.ts
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+app.listen(3000, () => {
+  console.log(`Express is running on 3000`);
+});
